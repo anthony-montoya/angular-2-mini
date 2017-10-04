e97a0ae05f3a4839ba5fae399f2a436506262175
Anthony Montoya <a.montoya1073@gmail.com>
2017-10-04 13:55:09 -0600

    Finished Angular 2 mini

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,11 @@
-angular.module('myApp').service('productSrvc', function() {
+angular.module('myApp').service('productsSrvc', function($http, $state) {
+
+    this.getShoeData = function() {
+        return $http.get('https://practiceapi.devmountain.com/products?category=shoes');
+    }
+
+    this.getSockData = function() {
+        return $http.get('https://practiceapi.devmountain.com/products?category=socks');
+    }
 
 });
